1d6b61173939c7337c38a4c871c10a0da92f603a
Prashant33221 <giriprashant329@gmail.com>
2026-06-07 23:51:42 +0530

    Fix spacy model install remove --user flag

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ try:
 except ImportError:
     subprocess.run([
         sys.executable, "-m", "pip", "install",
-        "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0-py3-none-any.whl",
-        "--user"
+        "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0-py3-none-any.whl"
     ])
 import streamlit as st
 import tempfile
